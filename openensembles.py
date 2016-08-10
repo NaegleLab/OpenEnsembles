e98f6d20f0b32128450a264017432dbb3e3fa327
Kristen Naegle <knaegle@seas.wustl.edu>
2016-08-10 09:49:55 -0500

    transform outlier cases committed

--- openensembles.py
+++ openensembles.py
@@ -26,7 +26,32 @@ class data:
         self.params['parent'] = []
 
         #check that the number of x-values matches the array
-        if(len(x) != X.shape[1]):
-            print "ERROR: Size of x-values (%d) does not match that of of the dataframe dimensions (%d), replacing with an vector of integers of correct size"%(len(x), X.shape[1])
-            self.x['parent'] = list(range(len(x)))
+        if(len(x) != self.D['parent'].shape[1]):
+            print "ERROR: Size of x-values (%d) does not match that of of the dataframe dimensions (%d), replacing with an vector of integers of correct size"%(len(x), self.D['parent'].shape[1])
+            self.x['parent'] = list(range(self.D['parent'].shape[1]))
 
+    def transform(self, source_name, txfm_fcn, txfm_name, var_params):
+        """
+        This runs transform (txfm_fcn) on the data matrix defined by
+        source_name with parameters that are variable for each transform. 
+        For example, oe.data.transform('parent', 'zscore','zscore_parent', []) will run the
+        zscore in a vector-wise manner across the matrix and the new data
+        dictionary access to the transformed data is oe.data['zscore_parent']
+        Returns an ERROR -1 if not performed. Successful completion results in
+        the addition of a new entry in the data dictionary with a key according
+        to txfm_name.
+        
+        """
+        txfm_fcn_dict = {'zscore':0}  
+        #CHECK that the source exists
+        if source_name not in self.D:
+            print "ERROR: the source you requested for transformation does not exist by that name %s"%(source_name)
+            return -1
+
+        if txfm_name == 'zscore':
+            return 1
+                 
+
+        else:
+            print "ERROR: the transform function you requested does not exist, currently the following are supported %s"%(txfm_fcn_dict.keys)
+            return -2
--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -7,9 +7,28 @@ import pandas as pd
 import openensembles as oe
 
 class TestFunctions(unittest.TestCase):
+
     def setUp(self):
         fileName = 'data_test.csv'
         df = pd.DataFrame.from_csv(fileName)
         x = [0, 5, 30]
-        self.data = oe(df, x)
-        
+        self.data = oe.data(df, x)
+
+    def test_correct_setup(self):
+        self.assertEqual((3,3), self.data.D['parent'].shape)
+        self.assertEqual(3, len(self.data.x['parent']))
+
+    def test_incorrect_setup(self):
+        fileName = 'data_test.csv'
+        df = pd.DataFrame.from_csv(fileName)
+        x = [0, 5, 10, 30]
+        data = oe.data(df, x)
+        self.assertEqual((3,3), data.D['parent'].shape)
+        self.assertEqual(3, len(data.x['parent']))
+
+    def test_transform_error_noSource(self):
+        self.assertEqual(-1, self.data.transform('parentZ', 'zscore', 'zscore_parentZ', []))
+
+    def test_transform_error_noTxfm(self):
+        self.assertEqual(-2, self.data.transform('parent', 'zscoreGobblyGoodk', 'zscore_parent', []))
+
