f749738d99e8864f76702d1c2609d216d53b0365
Kristen Naegle <knaegle@wustl.edu>
2017-12-28 10:27:57 -0600

    Added license to all files.

--- clustering_algorithms.py
+++ clustering_algorithms.py
@@ -3,6 +3,23 @@ OpenEnsembles is a resource for performing and analyzing ensemble clustering
 This file contains calls to clustering algorithms. Please refer to this 
 documentation for specifics about the variable parameters and their defaults, but interact with clustering 
 through the openensembles.clustering() class. 
+
+OpenEnsembles is a resource for performing and analyzing ensemble clustering
+
+Copyright (C) 2017 Naegle Lab
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 import numpy as np 
--- transforms.py
+++ transforms.py
@@ -4,7 +4,21 @@ This file contains all transform functions. Each transform takes a data matrix
 an x-vector and variable arguments. It also returns a data matrix, an x-vector
 and a dictionary of parameters used (name, value). 
 
-Refer to this documentation for 
+Copyright (C) 2017 Naegle Lab
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 """
 
 import numpy as np 
