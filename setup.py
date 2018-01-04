b1ebb2ca901efe768384a5915241472cf827142b
Shawn Anastasio <shawnanastasio@yahoo.com>
2018-01-04 15:02:20 -0600

    Add setup.py for packaging and installing library
    Signed-off-by: Shawn Anastasio <shawnanastasio@yahoo.com>

---
+++ setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup
+import os
+import shutil
+
+DESC = 'An open source Python package for performing, finishing, and analyzing ensemble clustering.'
+
+PACKAGE_FILES = [
+    '__init__.py',
+    'openensembles.py',
+    'clustering_algorithms.py',
+    'cooccurrence.py',
+    'finishing.py',
+    'transforms.py',
+    'validation.py'
+]
+
+# Create a temporary package directory and copy all src files to it
+os.mkdir('openensembles')
+for f in PACKAGE_FILES:
+    shutil.copy(f, 'openensembles/' + f)
+
+# Run setup
+setup(
+    name='OpenEnsembles',
+    version='0.9',
+    author='Naegle Lab',
+    author_email='see github',
+    url='https://github.com/NaegleLab/OpenEnsembles',
+    packages=['openensembles'],
+    install_requires=['pandas', 'numpy', 'sklearn'],
+    license='GNU General Public License v3',
+    summary=DESC,
+    long_description=DESC,
+)
+
+# Remove package directory
+shutil.rmtree('openensembles')
