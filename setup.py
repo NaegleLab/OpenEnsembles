cabe6aec11bb8b48f75901d66301167f81ebcdbf
Shawn Anastasio <shawnanastasio@yahoo.com>
2018-02-16 22:25:19 -0600

    Move all source files to separate directory and bump version to 1.0.1
    Restructure repository to match the traditional layout of python modules.
    Also update setup.py to reflect these changes.

--- setup.py
+++ setup.py
@@ -4,35 +4,16 @@ import shutil
 
 DESC = 'An open source Python package for performing, finishing, and analyzing ensemble clustering.'
 
-PACKAGE_FILES = [
-    '__init__.py',
-    'openensembles.py',
-    'clustering_algorithms.py',
-    'cooccurrence.py',
-    'finishing.py',
-    'transforms.py',
-    'validation.py',
-    'mutualinformation.py'
-]
-
-# Create a temporary package directory and copy all src files to it
-os.mkdir('openensembles')
-for f in PACKAGE_FILES:
-    shutil.copy(f, 'openensembles/' + f)
-
 # Run setup
 setup(
     name='OpenEnsembles',
-    version='1.0.0',
+    version='1.0.1',
     author='Naegle Lab',
-    author_email='see github',
     url='https://github.com/NaegleLab/OpenEnsembles',
     packages=['openensembles'],
-    install_requires=['pandas', 'numpy', 'sklearn'],
+    install_requires=['pandas', 'numpy', 'sklearn', 'scipy', 'networkx', 'matplotlib'],
     license='GNU General Public License v3',
     summary=DESC,
     long_description=DESC,
 )
 
-# Remove package directory
-shutil.rmtree('openensembles')
